9225a7af0a32ad5d6e102f528d245d3c692973bc
Kevin Smith <ksmith.nop@gmail.com>
2013-11-05 02:25:07 -0500

    ideas for method lookup

--- cplus.c
+++ cplus.c
@@ -5,15 +5,21 @@ class(Parent,
 )
 
 class(Child,
-	int num;
+// BEGIN() <- marker for function memory
 	FUNC foo;
 	FUNC bar;
+// END()
+	int num;
 )
 
 void p_ctor(this) {}
 void p_dtor(this) {}
 void foo(this, int a) {}
 void bar(this, int a) {}
+
+// defaulting values?!
+//#define FOOD_INIT(...) { .id = -1, .foo = -1, .bar = -1, ## __VA_ARGS__ }
+//struct food barf = FOO_INIT( .id = 42, .foo = 2 );
 
 void build_classes()
 {
